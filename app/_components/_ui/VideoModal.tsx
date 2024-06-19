1c550e459ac9e9d75e797924c24bf54b0dc38636
md imran <119500104+imranwebdev@users.noreply.github.com>
2024-06-19 23:43:14 +0600

    Update VideoModal.tsx

--- app/_components/_ui/VideoModal.tsx
+++ app/_components/_ui/VideoModal.tsx
@@ -1,62 +1,62 @@
-"use client"
+"use client";
 
 interface VideoModalProps {
-    show: boolean;
-    onClose: () => void;
+  show: boolean;
+  onClose: () => void;
 }
 
 const VideoModal: React.FC<VideoModalProps> = ({ show, onClose }) => {
-    if (!show) {
-        return null;
-    }
+  if (!show) {
+    return null;
+  }
 
-    return (
-        <div style={modalStyle}>
-            <div style={modalContentStyle} className="m-10 md:m-0">
-                <button style={closeButtonStyle} onClick={onClose}>×</button>
-                <iframe
-                className="w-[420px] h-[250px]   md:w-[560px] md:h-[315px]"
-                    width="560"
-                    height="315"
-                    src="https://www.youtube.com/embed/ZK-rNEhJIDs"
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                ></iframe>
-            </div>
-        </div>
-    );
+  return (
+    <div style={modalStyle} className="">
+      <div style={modalContentStyle} className="m-10 md:m-0 w-[80%]">
+        <button style={closeButtonStyle} onClick={onClose}>
+          ×
+        </button>
+        <iframe
+          className="w-full h-[250px]   md:w-[560px] md:h-[315px]"
+          src="https://www.youtube.com/embed/ZK-rNEhJIDs"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+  );
 };
 
 const modalStyle: React.CSSProperties = {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
-    backgroundColor: 'rgba(0,0,0,0.7)',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    zIndex: 1000,
+  position: "fixed",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+  backgroundColor: "rgba(0,0,0,0.7)",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  zIndex: 1000,
 };
 
 const modalContentStyle: React.CSSProperties = {
-    position: 'relative',
-    backgroundColor: '#fff',
-    padding: '36px',
-    borderRadius: '8px',
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+  position: "relative",
+  backgroundColor: "#fff",
+  padding: "50px 10px",
+  borderRadius: "8px",
+  boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
 };
 
 const closeButtonStyle: React.CSSProperties = {
-    position: 'absolute',
-    top: '10px',
-    right: '10px',
-    background: 'none',
-    border: 'none',
-    fontSize: '24px',
-    cursor: 'pointer',
+  position: "absolute",
+  top: "10px",
+  right: "10px",
+  background: "none",
+  border: "none",
+  fontSize: "24px",
+  cursor: "pointer",
 };
 
 export default VideoModal;
